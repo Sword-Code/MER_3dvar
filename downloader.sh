a90496ba297b837fddd5bd5786f0ab3c1b6befdb
Simone Spada <sspada@ogs.it>
2024-10-30 14:58:12 +0100

    fixed git correctly changing branch

--- downloader.sh
+++ downloader.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-VAR3D_RELEASE=Multivariate
+VAR3D_BRANCH=Multivariate
 
 
 HOME_3dvar=$PWD
@@ -8,4 +8,4 @@ HOME_3dvar=$PWD
 cd $HOME_3dvar
 git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
 cd 3DVar
-git checkout -b $VAR3D_RELEASE $VAR3D_RELEASE
+git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
