c24f01876f3ac1c78a9e5500f1c0c972cc292933
Simone Spada <sspada@ogs.it>
2024-11-05 17:34:39 +0100

    now create_var_list.f90 also creates DAlevels.txt with the number of 3dvar vertical levels

--- src/create_var_list.f90
+++ src/create_var_list.f90
@@ -1,20 +1,26 @@
 program create_var_list
-use da_params, only: DA_VarList, NBioVar, SET_DA_PARAMS, CLEAN_DA_PARAMS
+use da_params, only: DA_VarList, NBioVar, jpk_200, SET_DA_PARAMS, CLEAN_DA_PARAMS
 implicit none
 
 integer :: my_unit=86
 integer :: i
-character(len=10) :: file_name="DAvars.txt"
+character(len=10) :: DAvars_file_name="DAvars.txt"
+character(len=12) :: DAlevels_file_name="DAlevels.txt"
+
 
 call SET_DA_PARAMS
 
-OPEN(UNIT=my_unit, FILE=file_name, ACTION= 'WRITE')
+OPEN(UNIT=my_unit, FILE=DAvars_file_name, ACTION= 'WRITE')
     do i=1, NBioVar
         write(my_unit,*) DA_VarList(i)
     end do
 close(unit=my_unit)
+write(*,*) DAvars_file_name, ' written.'
 
-write(*,*) file_name, 'written.'
+OPEN(UNIT=my_unit, FILE=DAlevels_file_name, ACTION= 'WRITE')
+    write(my_unit,*) jpk_200
+close(unit=my_unit)
+write(*,*) DAlevels_file_name, ' written.'
 
 call CLEAN_DA_PARAMS
 
