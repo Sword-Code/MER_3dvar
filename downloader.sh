4728faf50fd1ed883da0cc12158d389ad249951d
Simone Spada <simonespada.info@gmail.com>
2025-02-25 18:14:26 +0100

    changed 3dvar repository to github

--- downloader.sh
+++ downloader.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 
 VAR3D_BRANCH=Multivariate
-
-
 HOME_3dvar=$PWD
 
 cd $HOME_3dvar
-git clone git@gitlab.hpc.cineca.it:OGS/3DVar.git
+git clone git@github.com:inogs/3dVarBio.git 3DVar
 cd 3DVar
 git checkout -b $VAR3D_BRANCH origin/$VAR3D_BRANCH
